4e2e1d35952b39f9113f9ffc9077249738c61839
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-09-29 22:07:33 +0800

    finish desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,15 @@ export default {
     theme: {
         extend: {
             colors: {
-
+                c_Dark_Violet: "hsl(256, 26%, 20%)",
+                c_Grayish_Blue: "hsl(216, 30%, 68%)",
+                c_Very_Dark_Violet: "hsl(270, 9%, 17%)",
+                c_Dark_Grayish_Violet: "hsl(273, 4%, 51%)",
+                c_Very_Light_Gray: "hsl(0, 0%, 98%)",
             },
             fontFamily: {
-
+                dm: ["DM Serif Display", "serif"],
+                karla: ["Karla", "system-ui"]
             }
         },
     },
